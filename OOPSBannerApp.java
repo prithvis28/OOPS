4470dbed605da5c33f0724ef51cb0b31b19aa4e1
prithvis28 <ps5252@srmist.edu.in>
2026-03-26 10:57:54 +0530

    UC5: Concise array initialization using String.join()

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,18 +1,26 @@
+/**
+ * OOPSBannerApp
+ *
+ * UC5 - Concise Array Initialization using String.join()
+ *
+ * @author Prithvi
+ * @version 5.0
+ */
 public class OOPSBannerApp {
 
     public static void main(String[] args) {
 
-        String[] bannerLines = {
-            String.join("  ", " ***** ", " ***** ", " ******", " ***** "),
-            String.join("  ", "*     *", "*     *", "*     *", "*     *"),
-            String.join("  ", "*     *", "*     *", "*     *", "*      "),
-            String.join("  ", "*     *", "*     *", "****** ", " ***** "),
-            String.join("  ", "*     *", "*     *", "*      ", "      *"),
-            String.join("  ", "*     *", "*     *", "*      ", "*     *"),
-            String.join("  ", " ***** ", " ***** ", "*      ", " ***** ")
+        String[] banner = {
+            String.join("   ", " ***** ", " ***** ", " ***** ", " ***** "),
+            String.join("   ", "*     *", "*     *", "*     *", "*      "),
+            String.join("   ", "*     *", "*     *", "*     *", "*      "),
+            String.join("   ", "*     *", "*     *", "*     *", " ***** "),
+            String.join("   ", "*     *", "*     *", "*     *", "      *"),
+            String.join("   ", "*     *", "*     *", "*     *", "      *"),
+            String.join("   ", " ***** ", " ***** ", " ***** ", " ***** ")
         };
 
-        for (String line : bannerLines) {
+        for (String line : banner) {
             System.out.println(line);
         }
     }
